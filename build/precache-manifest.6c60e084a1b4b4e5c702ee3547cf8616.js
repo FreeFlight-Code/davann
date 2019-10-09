5bda28ae707af62bf84612ad672123e695e833a6
David Fischer <david@simustream.com>
2019-10-09 10:06:59 -0600

    build

---
+++ build/precache-manifest.6c60e084a1b4b4e5c702ee3547cf8616.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "40442936d694ec342267",
+    "url": "/static/js/main.58dd7db9.chunk.js"
+  },
+  {
+    "revision": "0640aa10858f2a5d5632",
+    "url": "/static/js/2.186e26a6.chunk.js"
+  },
+  {
+    "revision": "40442936d694ec342267",
+    "url": "/static/css/main.6ac998c4.chunk.css"
+  },
+  {
+    "revision": "520e2b9742c12bb86e113093d8e48fcf",
+    "url": "/index.html"
+  }
+];
