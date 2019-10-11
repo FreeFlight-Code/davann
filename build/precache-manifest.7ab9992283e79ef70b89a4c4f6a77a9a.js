14101af60ea930758b16ad5ae01dece2d1fce402
FreeFlight-Code <davidfisc@hotmail.com>
2019-10-10 21:16:32 -0600

    small update

---
+++ build/precache-manifest.7ab9992283e79ef70b89a4c4f6a77a9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3581c030ee16f658ab84e68c3e530039",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7edebef13b3ca04053a1",
+    "url": "/static/css/main.5e9cba0f.chunk.css"
+  },
+  {
+    "revision": "29814285a2cfab3d8c0a",
+    "url": "/static/js/2.2c467789.chunk.js"
+  },
+  {
+    "revision": "7edebef13b3ca04053a1",
+    "url": "/static/js/main.6a8f0cc6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
